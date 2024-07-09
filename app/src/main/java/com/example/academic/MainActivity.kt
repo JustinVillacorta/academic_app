7cb01f6ba258f14f3819cd762a7180694eae0ae0
JustinVillacorta <juvi.villacorta.up@phinmaed.com>
2024-07-09 20:26:11 +0800

    pa check

--- app/src/main/java/com/example/academic/MainActivity.kt
+++ app/src/main/java/com/example/academic/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, MainActivity2::class.java)
                 startActivity(intent)
                 //gumagana
+                //testing
             }
         }
 
