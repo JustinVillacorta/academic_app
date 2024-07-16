3576f1818993e9e8b04d103e1342a07ee76dd350
JustinVillacorta <juvi.villacorta.up@phinmaed.com>
2024-07-16 13:53:46 +0800

    App drawer fix

--- app/src/main/java/com/example/academic/MainActivity2.kt
+++ app/src/main/java/com/example/academic/MainActivity2.kt
@@ -6,47 +6,45 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
 import com.example.academic.databinding.ActivityMain2Binding
-import com.example.academic.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+
+        binding.bottomNavigationView.selectedItemId = R.id.dashboard
         replaceFragment(dashboard())
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.dashboard -> replaceFragment(dashboard())
                 R.id.exams -> replaceFragment(Exams())
                 R.id.grades -> replaceFragment(grades())
                 R.id.profile -> replaceFragment(Profile())
                 R.id.quiz -> replaceFragment(quiz())
-
-                else ->{
-
-                }
-
+                else -> { }
             }
             true
         }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
     }
 
-    private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
 }
