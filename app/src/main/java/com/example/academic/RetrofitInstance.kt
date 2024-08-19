f35a93598e5e14b1813147440cb016f6a986fda4
jericko <jerickogarcia0@gmail.com>
2024-08-19 11:27:41 +0800

    network security

--- app/src/main/java/com/example/academic/RetrofitInstance.kt
+++ app/src/main/java/com/example/academic/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.1.11/") // Make sure this URL points to your local server
+            .baseUrl("http://192.168.1.12/") // Make sure this URL points to your local server
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
